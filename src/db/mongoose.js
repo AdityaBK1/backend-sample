5c77864456eb1cf395ded53f28a03065485c5a12
Aditya <131005390+AdityaBK1@users.noreply.github.com>
2023-08-07 12:15:02 +0530

    Add files via upload

---
+++ src/db/mongoose.js
@@ -0,0 +1,9 @@
+import mongoose from 'mongoose'
+import 'dotenv/config'
+import chalk from "chalk";
+
+mongoose.connect( process.env.MONGO_URI, {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true }
+).then(() => console.log(chalk.yellow.bold.underline('database connection established')))
+.catch(err => console.log(err))
